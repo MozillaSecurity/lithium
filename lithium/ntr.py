530c42991ebc7616ccfe67584019fcaf9981d773
Jesse Ruderman <jruderman@gmail.com>
2008-10-10 09:50:32 -0700

    Switch ntr.py from fork/execvp to subprocess.Popen.  This seems to fix the mysterious firefox-bin process leak on Leopard.

--- lithium/ntr.py
+++ lithium/ntr.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python -u
 
-import os, signal, sys, time, platform
+import os, signal, sys, time, platform, subprocess
 
 exitBadUsage = 2
-exitOSError   = 66
-exitInterrupt = 99
 
 (CRASHED, TIMED_OUT, NORMAL, ABNORMAL, NONE) = range(5)
-
-pid = None
 
 
 def getSignalName(num):
@@ -17,41 +13,6 @@ def getSignalName(num):
             if getattr(signal, p) == num:
                 return p
     return "Unknown signal"
-
-
-def alarm_handler(signum, frame):
-    global pid
-    try:
-        os.kill(pid, signal.SIGKILL)
-    except:
-        pass
-
-
-def forkexec(commandWithArgs, logPrefix):
-    newpid = os.fork()
-    if newpid == 0: # Child
-
-        try:
-            # Redirect stdout, just for the child.
-            so = file(logPrefix + "-out", 'w')
-            os.dup2(so.fileno(), sys.stdout.fileno())
-
-            # Redirect stderr, to a separate file.
-            se = file(logPrefix + "-err", 'w')
-            os.dup2(se.fileno(), sys.stderr.fileno())
-            
-            # Transfer control of the child to the target program
-            # Its argv[0] wants to be the same as its path, so...
-            os.execvp(commandWithArgs[0], commandWithArgs)
-             
-        except OSError, e:
-            print "ERROR: %s failed: %d (%s)" % (repr(commandWithArgs), e.errno, e.strerror)
-            # Note that we can only make the child process exit from here!
-            # Use a special exit code (HACK!) to tell the parent to exit, too.
-            sys.exit(exitOSError)
-
-    else:  # Parent
-        return newpid
 
 
 def timed_run(commandWithArgs, timeout, logPrefix):
@@ -73,58 +34,53 @@ def timed_run(commandWithArgs, timeout, logPrefix):
         print "I think you want firefox-bin!"
         sys.exit(exitBadUsage)
         
-        
-    global pid
-    global CRASHED, TIMED_OUT, NORMAL, ABNORMAL, NONE
-    
-    signal.signal(signal.SIGALRM, alarm_handler)
-    signal.alarm(timeout)
-
     starttime = time.time()
     
     sta = NONE
     msg = ''
 
-    pid = forkexec(commandWithArgs, logPrefix)
+    childStdOut = file(logPrefix + "-out", 'w')
+    childStdErr = file(logPrefix + "-err", 'w')
+    child = subprocess.Popen(commandWithArgs, stderr = childStdErr, stdout = childStdOut)
 
-    try: 
-      status = os.waitpid(pid, 0)[1]
-    except OSError:
-      # Guess that it timed out (waitpid throws "Interrupted system call" in this case!?)
-      msg = 'TIMED OUT'
-      sta = TIMED_OUT
-    except KeyboardInterrupt:
-      # Depending on how ntr.py was invoked, calling 'print' might throw an IOError, "Broken pipe"?!
-      print "Bye!"
-      try:
-        os.kill(pid, signal.SIGKILL)
-      except OSError, e:
-        print "Unable to kill it: %s" % e
-      sys.exit(exitInterrupt)
-    
-    signal.alarm(0) # Cancel the alarm
+    killed = False
 
-    stoptime = time.time()
-    elapsedtime = stoptime - starttime
-    
-    if (sta == TIMED_OUT):
-        pass
-    elif os.WIFEXITED(status):
-        rc = os.WEXITSTATUS(status)
-        if rc == 0:
-            msg = 'NORMAL'
-            sta = NORMAL
+    while 1: 
+        rc = child.poll()
+        elapsedtime = time.time() - starttime
+        if rc == None:
+            if elapsedtime > timeout and not killed:
+                # To be replaced with 'child.kill' when Python 2.6 is available,
+                # since os.kill doesn't work on Windows.
+                os.kill(child.pid, signal.SIGKILL)
+                # but continue looping, because maybe kill takes a few seconds or maybe it's busy crashing!
+                killed = True
+            else:
+                time.sleep(0.100)
         else:
-            msg = 'ABNORMAL ' + str(rc)
-            sta = ABNORMAL
-    elif os.WIFSIGNALED(status):
-        signum = os.WTERMSIG(status)
+            break
+    
+    # Am I supposed to do this?
+    childStdOut.close()
+    childStdErr.close()
+
+    if rc == -signal.SIGKILL and killed:
+        msg = 'TIMED OUT'
+        sta = TIMED_OUT
+    elif rc == 0:
+        msg = 'NORMAL'
+        sta = NORMAL
+    elif rc > 0:
+        msg = 'ABNORMAL return code ' + str(rc)
+        sta = ABNORMAL
+    else:
+        # rc < 0
+        # The program was terminated by a signal, which usually indicates a crash.
+        # Mac/Linux only!
+        signum = -rc
         msg = 'CRASHED signal %d (%s)' % (signum, getSignalName(signum))
         sta = CRASHED
-        grabCrashLog(progname, pid, logPrefix, signum)
-    else:
-        msg = 'NONE'
-        sta = NONE
+        grabCrashLog(progname, child.pid, logPrefix, signum)
 
     return (sta, msg, elapsedtime)
 
