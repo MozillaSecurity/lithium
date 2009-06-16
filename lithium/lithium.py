d138d4e8df5523f4e5aad1f2900d04914f75e1e4
Jesse Ruderman <jruderman@gmail.com>
2009-06-16 15:25:38 -0700

    Allow Lithium to use condition scripts from any directory

--- lithium/lithium.py
+++ lithium/lithium.py
@@ -89,13 +89,8 @@ def main():
 
     if testcaseFilename == None:
         usageError("No testcase specified (use --testcase or last condition arg)")
-    
-    conditionF = args[0]
-    if conditionF.startswith("./"):
-        conditionF = conditionF[2:]
-    if conditionF.endswith(".py"):
-        conditionF = conditionF[:-3]
-    conditionScript = __import__(conditionF)
+
+    conditionScript = importRelativeOrAbsolute(args[0])
     conditionArgs = args[1:]
 
     if hasattr(conditionScript, "init"):
@@ -502,7 +497,21 @@ def quantity(n, s):
     if n != 1:
         r += "s"
     return r
-    
+
+def importRelativeOrAbsolute(f):
+    # maybe there's a way to do this more sanely with the |imp| module...
+    if f.endswith(".py"):
+        f = f[:-3]
+    if f.rfind(os.path.sep):
+        # Add the path part of the filename to the import path
+        (p, _, f) = f.rpartition(os.path.sep)
+        sys.path.append(p)
+    else:
+        # Add working directory to the import path
+        sys.path.append(".")
+    module = __import__(f)
+    del sys.path[0]
+    return module
 
 # Run main
 
