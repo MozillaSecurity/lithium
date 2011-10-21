01a7cf0ce150b626dd6f96aedb334da0bd84d909
Gary Kwong <gary@rumblingedge.com>
2011-10-21 13:20:30 -0700

    At long last, add crashesat_timed.py to lithium modules.

---
+++ lithium/crashesat_timed.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+import ntr
+
+def filecontains(f, s):
+   for line in f:
+       if line.find(s) != -1:
+           return True
+   return False
+
+
+def interesting(args, tempPrefix):
+    timeout = int(args[0])
+    desiredCrashSignature = args[1]
+
+    runinfo = ntr.timed_run(args[2:], timeout, tempPrefix)
+
+    timeString = " (%.3f seconds)" % runinfo.elapsedtime
+
+    crashLogName = tempPrefix + "-crash"
+
+    if runinfo.sta == ntr.CRASHED:
+        if os.path.exists(crashLogName):
+            if filecontains(file(crashLogName), desiredCrashSignature):
+                print "[CrashesAt] It crashed in " + desiredCrashSignature + " :)" + timeString
+                return True
+            else:
+                print "[CrashesAt] It crashed somewhere else!" + timeString
+                return False
+        else:
+            print "[CrashesAt] It appeared to crash, but no crash log was found?" + timeString
+            return False
+    else:
+        print "[CrashesAt] It didn't crash." + timeString
+        return False
