061a31a3c8419c2ba9fd11c8ec13964354d0425c
Jesse Ruderman <jruderman@gmail.com>
2015-05-29 15:34:25 -0700

    Happify linter

--- interestingness/outputs.py
+++ interestingness/outputs.py
@@ -14,14 +14,15 @@ def parseOptions(arguments):
     parser = OptionParser()
     parser.disable_interspersed_args()
     parser.add_option('-t', '--timeout', type='int', action='store', dest='condTimeout',
-                      default = 120,
+                      default=120,
                       help='Optionally set the timeout. Defaults to 120 seconds.')
     parser.add_option('-r', '--regex', action='store_true', dest='useRegex',
-                      default = False,
-                      help = 'Allow search for regular expressions instead of strings.')
+                      default=False,
+                      help='Allow search for regular expressions instead of strings.')
     options, args = parser.parse_args(arguments)
 
     return options.condTimeout, options.useRegex, args
+
 
 def interesting(cliArgs, tempPrefix):
     (timeout, regexEnabled, args) = parseOptions(cliArgs)
@@ -30,8 +31,10 @@ def interesting(cliArgs, tempPrefix):
 
     runinfo = timedRun.timed_run(args[1:], timeout, tempPrefix)
 
-    result = fileIngredients.fileContains(tempPrefix + "-out.txt", searchFor, regexEnabled)[0] or \
-             fileIngredients.fileContains(tempPrefix + "-err.txt", searchFor, regexEnabled)[0]
+    result = (
+        fileIngredients.fileContains(tempPrefix + "-out.txt", searchFor, regexEnabled)[0] or
+        fileIngredients.fileContains(tempPrefix + "-err.txt", searchFor, regexEnabled)[0]
+    )
 
     print 'Exit status: ' + runinfo.msg + " (%.3f seconds)" % runinfo.elapsedtime
 
