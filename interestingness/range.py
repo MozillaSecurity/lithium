b88d6a6971982f0df31bb95ca3723ccf68358ab0
Gary Kwong <gary@rumblingedge.com>
2017-07-13 18:26:54 -0700

    Convert existing file header comments to module docstrings for interestingness/range.py and interestingness/ximport.py

--- interestingness/range.py
+++ interestingness/range.py
@@ -1,32 +1,34 @@
 #!/usr/bin/env python
 # coding=utf-8
-# pylint: disable=invalid-name,missing-docstring
+# pylint: disable=invalid-name
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-# Repeats an interestingness test a given number of times.
-# If "RANGENUM" is present, it is replaced in turn with each number in the range.
-#
-# Use for:
-#
-# 1. Intermittent testcases.
-#
-#    Repeating the test can make the bug occur often enough for Lithium to make progress.
-#
-#     lithium.py range 1 20 crashes --timeout=3 ./js-dbg-32-mozilla-central-linux -m -n intermittent.js
-#
-# 2. Unstable testcases.
-#
-#    Varying a number in the test (using RANGENUM) may allow other parts of the testcase to be
-#    removed (Lithium), or may allow different versions of the shell to crash (autoBisect).
-#
-#    In the testcase:
-#      schedulegc(n);
-#
-#    On the command line:
-#      lithium.py range 1 50 crashes --timeout=3 ./js-dbg-32-mozilla-central-linux -e "n=RANGENUM;" 740654.js
+"""
+Repeats an interestingness test a given number of times.
+If "RANGENUM" is present, it is replaced in turn with each number in the range.
+
+Use for:
+
+1. Intermittent testcases.
+
+   Repeating the test can make the bug occur often enough for Lithium to make progress.
+
+    lithium.py range 1 20 crashes --timeout=3 ./js-dbg-32-mozilla-central-linux -m -n intermittent.js
+
+2. Unstable testcases.
+
+   Varying a number in the test (using RANGENUM) may allow other parts of the testcase to be
+   removed (Lithium), or may allow different versions of the shell to crash (autoBisect).
+
+   In the testcase:
+     schedulegc(n);
+
+   On the command line:
+     lithium.py range 1 50 crashes --timeout=3 ./js-dbg-32-mozilla-central-linux -e "n=RANGENUM;" 740654.js
+"""
 
 from __future__ import absolute_import, print_function
 
@@ -35,7 +37,7 @@ from optparse import OptionParser  # pylint: disable=deprecated-module
 from . import ximport
 
 
-def parseOptions(arguments):
+def parseOptions(arguments):  # pylint: disable=missing-docstring
     parser = OptionParser()
     parser.disable_interspersed_args()
     _options, args = parser.parse_args(arguments)
@@ -43,7 +45,7 @@ def parseOptions(arguments):
     return int(args[0]), int(args[1]), args[2:]  # args[0] is minLoopNum, args[1] maxLoopNum
 
 
-def interesting(cliArgs, tempPrefix):
+def interesting(cliArgs, tempPrefix):  # pylint: disable=missing-docstring
     (rangeMin, rangeMax, arguments) = parseOptions(cliArgs)
     conditionScript = ximport.importRelativeOrAbsolute(arguments[0])
     conditionArgs = arguments[1:]
--- interestingness/ximport.py
+++ interestingness/ximport.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # coding=utf-8
-# pylint: disable=invalid-name,missing-docstring
+# pylint: disable=invalid-name
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-# This lets you import an interestingness test given a full path, or given just a filename
-# (assuming it's in the current directory OR in the same directory as ximport)
+"""
+This lets you import an interestingness test given a full path, or given just a filename.
+(assuming it's in the current directory OR in the same directory as ximport)
+"""
 
 from __future__ import absolute_import
 
@@ -18,7 +20,7 @@ import sys
 log = logging.getLogger("lithium")
 
 
-def importRelativeOrAbsolute(f):
+def importRelativeOrAbsolute(f):  # pylint: disable=missing-docstring
     # maybe there's a way to do this more sanely with the |imp| module...
     if f.endswith(".py"):
         f = f[:-3]
