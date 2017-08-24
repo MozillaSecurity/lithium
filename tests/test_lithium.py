68b880f04854d09c2cd145e1d28c93ba28bc7c5f
Gary Kwong <gary@rumblingedge.com>
2017-08-24 15:20:58 -0700

    Move tests/ out of the src/lithium/ directory.

--- tests/test_lithium.py
+++ tests/test_lithium.py
@@ -18,7 +18,7 @@ import sys
 import tempfile
 import unittest
 
-from .. import reducer
+from lithium import reducer
 
 log = logging.getLogger("lithium_test")
 logging.basicConfig(level=logging.DEBUG)
@@ -296,7 +296,7 @@ class LithiumTests(TestCase):
         self.assertIn("INFO:lithium:The file has 0 lines so there's nothing for Lithium to try to remove!", logs.output)
 
     def test_arithmetic(self):
-        path = os.path.join(os.path.dirname(__file__), os.pardir, "docs", "examples", "arithmetic")
+        path = os.path.join(os.path.dirname(__file__), os.pardir, "src", "lithium", "docs", "examples", "arithmetic")
         shutil.copyfile(os.path.join(path, "11.txt"), "11.txt")
         result = reducer.Lithium().main([os.path.join(path, "product_divides.py"), "35", "11.txt"])
         self.assertEqual(result, 0)
