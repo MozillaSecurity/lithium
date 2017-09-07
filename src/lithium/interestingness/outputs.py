b84eccbc855bae178109ddfb187603d5a0daacfa
Gary Kwong <gary@rumblingedge.com>
2017-09-06 21:04:45 -0700

    Convert "outputs" test to use argparse and update documentation.

--- src/lithium/interestingness/outputs.py
+++ src/lithium/interestingness/outputs.py
@@ -1,48 +1,60 @@
 #!/usr/bin/env python
 # coding=utf-8
-# pylint: disable=missing-docstring
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-from __future__ import absolute_import, print_function
+"""Lithium's "outputs" interestingness test to assess whether an intended message shows up.
 
-import optparse  # pylint: disable=deprecated-module
+Example:
+    python -m lithium outputs --timeout=9 FOO ./js --fuzzing-safe testcase.js
+"""
+
+from __future__ import absolute_import
+
+import argparse
+import logging
 import sys
 
-from . import utils
 from . import timed_run
+from . import utils
 
 
-def parse_options(arguments):  # pylint: disable=missing-docstring,missing-return-doc,missing-return-type-doc
-    parser = optparse.OptionParser()
-    parser.disable_interspersed_args()
-    parser.add_option("-t", "--timeout", type="int", action="store", dest="condTimeout",
-                      default=120,
-                      help="Optionally set the timeout. Defaults to 120 seconds.")
-    parser.add_option("-r", "--regex", action="store_true", dest="useRegex",
-                      default=False,
-                      help="Allow search for regular expressions instead of strings.")
-    options, args = parser.parse_args(arguments)
+def interesting(cli_args, temp_prefix):
+    """Interesting if the binary causes an intended message to show up. (e.g. on stdout/stderr)
 
-    return options.condTimeout, options.useRegex, args
+    Args:
+        cli_args (list): List of input arguments.
+        temp_prefix (str): Temporary directory prefix, e.g. tmp1/1 or tmp4/1
 
+    Returns:
+        bool: True if the intended message shows up, False otherwise.
+    """
+    parser = argparse.ArgumentParser(prog="outputs",
+                                     usage="python -m lithium %(prog)s [options] output_message binary [flags] "
+                                     "testcase.js")
+    parser.add_argument("-r", "--regex", action="store_true", default=False, dest="regex_enabled",
+                        help="Allow search for regular expressions instead of strings.")
+    parser.add_argument("-t", "--timeout", default=120, dest="timeout", type=int,
+                        help="Set the timeout. Defaults to '%(default)s' seconds.")
+    parser.add_argument("cmd_with_flags", nargs=argparse.REMAINDER)
+    args = parser.parse_args(cli_args)
 
-def interesting(cli_args, temp_prefix):  # pylint: disable=missing-docstring,missing-return-doc,missing-return-type-doc
-    (timeout, regex_enabled, args) = parse_options(cli_args)
+    logger = logging.getLogger(__name__)  # __name__ should be lithium.interestingness.outputs
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    logging.getLogger("flake8").setLevel(logging.WARNING)
 
-    search_for = args[0]
+    search_for = args.cmd_with_flags[0]
     if not isinstance(search_for, bytes):
         search_for = search_for.encode(sys.getfilesystemencoding())
 
-    runinfo = timed_run.timed_run(args[1:], timeout, temp_prefix)
-
+    # Run the program with desired flags and search stdout and stderr for intended message
+    runinfo = timed_run.timed_run(args.cmd_with_flags[1:], args.timeout, temp_prefix)
     result = (
-        utils.file_contains(temp_prefix + "-out.txt", search_for, regex_enabled)[0] or
-        utils.file_contains(temp_prefix + "-err.txt", search_for, regex_enabled)[0]
+        utils.file_contains(temp_prefix + "-out.txt", search_for, args.regex_enabled)[0] or
+        utils.file_contains(temp_prefix + "-err.txt", search_for, args.regex_enabled)[0]
     )
 
-    print("Exit status: %s (%.3f seconds)" % (runinfo.msg, runinfo.elapsedtime))
-
+    logger.info("Exit status: %s (%.3f seconds)", runinfo.msg, runinfo.elapsedtime)
     return result
