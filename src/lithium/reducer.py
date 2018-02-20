acc93a071f6df4be7b9a459b24a383d8ca3921ca
pyoor <pyoor@users.noreply.github.com>
2018-02-20 16:53:51 -0500

    Ignore pylint anomalous-backslash-in-string to allow python2/3 compatibility

--- src/lithium/reducer.py
+++ src/lithium/reducer.py
@@ -1226,7 +1226,7 @@ class CollapseEmptyBraces(Minimize):
             bool: True if callback was performed successfully, False otherwise.
         """
         raw = b"".join(testcase.parts)
-        modified = re.sub(b'{\s+}', b'{ }', raw)
+        modified = re.sub(b'{\s+}', b'{ }', raw)  # pylint: disable=anomalous-backslash-in-string
 
         # Don't update the testcase if no changes were applied
         if raw != modified:
