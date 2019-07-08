96f63a53515302c84936cb73eeb7cb06ea99780d
Jesse Schwartzentruber <truber@mozilla.com>
2019-07-08 16:45:29 -0400

    Bump version to 0.3.3

--- setup.py
+++ setup.py
@@ -42,5 +42,5 @@ if __name__ == "__main__":
             "lithium.interestingness",
         ],
         url="https://github.com/MozillaSecurity/lithium",
-        version="0.3.2",
+        version="0.3.3",
         zip_safe=False)
