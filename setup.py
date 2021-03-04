efaa61f5b334efd09262cd23f4d36f594b7e2615
Jesse Schwartzentruber <truber@mozilla.com>
2021-03-04 16:07:00 -0500

    Update CI configuration

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
-"""Lithium setup script"""
+"""setuptools install script"""
 
 import site
 
